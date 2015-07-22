cf4378a5298ee83f738bd3ca8c2c0b108d9c4071
Nils Neumann <neumann@placeworkers.com>
2015-07-22 15:48:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kirby Resources
+
+This is a collection of my personal resources like snippets, templates and plugins for my Kirby projects. 
---
+++ site/snippets/schemaorg_logo.php
@@ -0,0 +1,5 @@
+<h1 itemscope itemtype="http://schema.org/Organization">
+  <a itemprop="url" href="<?= url() ?>">
+    <img itemprop="logo" src="<?= url('assets/images/logo.png'); ?>" alt="<?= h($site->title()) ?>" width="500px" />
+  </a>
+</h1>
